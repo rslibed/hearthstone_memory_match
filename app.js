70a21da495fe11af704f4b8f1814ba7978702aec
rslibed <rslibed@gmail.com>
2017-11-01 16:24:44 -0700

    Still needs further development in regards to correct card flip matches and mismatches

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ var total_possible_matches = 2;
 var match_counter = 0;
 function initializeApp () {
     $(".card").click(card_clicked);
-
     function card_clicked () {
+        var firstImageSrc;
+        var secondImageSrc;
         if (first_card_clicked === null) {
             first_card_clicked = this;
             $(first_card_clicked).find(".back").hide();
         } else {
             second_card_clicked = this;
             $(second_card_clicked).find(".back").hide();
-            if ($(first_card_clicked).find(".front").find("img").attr("src") === $(second_card_clicked).find(".front").find("img").attr("src")) {
+            firstImageSrc = $(first_card_clicked).find(".front").find("img").attr("src");
+            secondImageSrc = $(second_card_clicked).find(".front").find("img").attr("src");
+            if (firstImageSrc === secondImageSrc) {
                 match_counter++;
             } else {
                 function hideIn2Seconds () {
-                    $(first_card_clicked).find(".back").show();
-                    $(second_card_clicked).find(".back").show();
-                    first_card_clicked = null;
-                    second_card_clicked = null;
+                    $(first_card_clicked).find(".back").hide();
+                    $(second_card_clicked).find(".back").hide();
                 }
                 setTimeout(hideIn2Seconds, 2000);
             }
+            first_card_clicked = null;
+            second_card_clicked = null;
+            firstImageSrc = null;
+            secondImageSrc = null;
         }
     }
 };
