2ab4898c3ed5b187192bc708b8be77b78375e8bc
rslibed <rslibed@gmail.com>
2017-11-01 14:02:24 -0700

    Start functionality, still need to fix bugs on matching card regarding show/hide the right elements at the right time

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ var total_possible_matches = 2;
 var match_counter = 0;
 function initializeApp () {
     $(".card").click(card_clicked);
+
     function card_clicked () {
         if (first_card_clicked === null) {
             first_card_clicked = this;
-            return;
+            $(first_card_clicked).find(".back").hide();
         } else {
             second_card_clicked = this;
-            // if () {
-            //
-            // }
-            return;
+            $(second_card_clicked).find(".back").hide();
+            if ($(first_card_clicked).find(".front").find("img").attr("src") === $(second_card_clicked).find(".front").find("img").attr("src")) {
+                match_counter++;
+            } else {
+                function hideIn2Seconds () {
+                    $(first_card_clicked).find(".back").show();
+                    $(second_card_clicked).find(".back").show();
+                    first_card_clicked = null;
+                    second_card_clicked = null;
+                }
+                setTimeout(hideIn2Seconds, 2000);
+            }
         }
     }
 };
