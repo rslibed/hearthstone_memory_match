481a5aeb208af00babe256bef8efb2c6fa101aab
rslibed <rslibed@gmail.com>
2017-11-01 19:53:30 -0700

    Cards can be flipped, cards that are matched will stay flipped and cards that are mismatched will flip back to original position, game prevents user from clicking on additional cards during the 2 second delay of mismatched cards flipping back down

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 $(document).ready(initializeApp);
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
+var preventClick = true;
 function initializeApp () {
     $(".card").click(card_clicked);
     function card_clicked () {
-        var firstImageSrc;
-        var secondImageSrc;
-        if (first_card_clicked === null) {
-            first_card_clicked = this;
-            $(first_card_clicked).find(".back").hide();
-        } else {
-            second_card_clicked = this;
-            $(second_card_clicked).find(".back").hide();
-            firstImageSrc = $(first_card_clicked).find(".front").find("img").attr("src");
-            secondImageSrc = $(second_card_clicked).find(".front").find("img").attr("src");
-            if (firstImageSrc === secondImageSrc) {
-                match_counter++;
+        if (preventClick) {
+            preventClick = false;
+
+            if (first_card_clicked === null) {
+                first_card_clicked = this;
+                $(first_card_clicked).find(".back").hide();
+                preventClick = true;
             } else {
-                function hideIn2Seconds () {
-                    $(first_card_clicked).find(".back").hide();
-                    $(second_card_clicked).find(".back").hide();
+                second_card_clicked = this;
+                $(second_card_clicked).find(".back").hide();
+                if ($(first_card_clicked).find(".front").find("img").attr("src") === $(second_card_clicked).find(".front").find("img").attr("src")) {
+                    match_counter++;
+                    preventClick = true;
+                    if (match_counter === total_possible_matches) {
+                        $("#stats-container .game-info p").text("You win!");
+                    }
+                    first_card_clicked = null;
+                    second_card_clicked = null;
+                } else {
+                    hideIn2Seconds();
                 }
-                setTimeout(hideIn2Seconds, 2000);
             }
-            first_card_clicked = null;
-            second_card_clicked = null;
-            firstImageSrc = null;
-            secondImageSrc = null;
+
+            function hideIn2Seconds() {
+                setTimeout(function () {
+                    $(first_card_clicked).find(".back").show();
+                    $(second_card_clicked).find(".back").show();
+                    preventClick = true;
+                    first_card_clicked = null;
+                    second_card_clicked = null;
+                }, 2000);
+            }
         }
     }
 };
