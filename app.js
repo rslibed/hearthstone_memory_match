eead5c0bc3bc40f2c1a906c0aca62f17f53a4eff
rslibed <rslibed@gmail.com>
2017-11-01 23:10:58 -0700

    Added global variables for stats container. Specifically match counter, number of matches, number of attempts, accuracy, and the number of games played

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var second_card_clicked = null;
 var total_possible_matches = 9;
 var match_counter = 0;
 var preventClick = true;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
+
 function initializeApp () {
     $(".card").click(card_clicked);
     function card_clicked () {
@@ -14,11 +19,18 @@ function initializeApp () {
                 first_card_clicked = this;
                 $(first_card_clicked).find(".back").hide();
                 preventClick = true;
+                if (preventClick) {
+                    $(this).stopPropagation();
+                }
             } else {
                 second_card_clicked = this;
                 $(second_card_clicked).find(".back").hide();
+                attempts++;
+                console.log("Attempts: " + attempts);
                 if ($(first_card_clicked).find(".front").find("img").attr("src") === $(second_card_clicked).find(".front").find("img").attr("src")) {
                     match_counter++;
+                    matches++;
+                    console.log("Matches: " + matches);
                     preventClick = true;
                     if (match_counter === total_possible_matches) {
                         $("#stats-container .game-info p").text("You win!");
@@ -40,5 +52,6 @@ function initializeApp () {
                 }, 2000);
             }
         }
+
     }
 };
